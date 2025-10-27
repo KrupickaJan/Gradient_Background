1e8a8929eb14d0fc809ada32cd75c5f078db124b
Krupicka <jkrupickaj@gmail.com>
2025-10-27 20:19:07 +0100

    Stop tracking dist directory

--- dist/prefs.d.ts
+++
@@ -1,7 +0,0 @@
-import Adw from 'gi://Adw';
-import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
-export default class extends ExtensionPreferences {
-    #private;
-    fillPreferencesWindow(window: Adw.PreferencesWindow): Promise<void>;
-}
-//# sourceMappingURL=prefs.d.ts.map
